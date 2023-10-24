e8526edf25010c17a60dbd0cc9ecff92dbb51923
hingesrushti <90634472+hingesrushti@users.noreply.github.com>
2023-10-24 19:52:41 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:personal_finance_app/data/model/add_date.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:personal_finance_app/widgets/bottomnavigationbar.dart';
+
+void main() async{
+  await Hive.initFlutter();
+  Hive.registerAdapter(AdddataAdapter());
+  await Hive.openBox<Add_data>('data');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Bottom(),
+    );
+  }
+}
